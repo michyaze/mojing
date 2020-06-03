765ce9c6aa87e63c26719c7d5d34a58df58e4734
cxf1633 <cxf1633@gmail.com>
2020-06-04 02:06:54 +0800

    添加vscode调试 添加数据读取

--- cocos_mojing/assets/script/loadConfig.js
+++ cocos_mojing/assets/script/loadConfig.js
@@ -31,18 +31,20 @@ cc.Class({
     // onLoad () {},
 
     start () {
-        cc.loader.loadRes('config/config.json', function (err, object) {
+        cc.log("从本地加载数据：");
+        this._config = {};
+        cc.loader.loadRes('config/config', function (err, object) {
             if (err) {
-                cc.log("err!!!!!!!!!!!!");
-                console.log(err);
+                cc.log("load config error!!");
                 return;
             }
-            cc.log("loadRes");
             // 读取的数据返回在object中，这是一个拥有2个元素的对象
             let engine = object.json.engine; // engine = box2D
             let ver = object.json.version;   // ver = 2.3.1
             cc.log("engine = " + engine);
             cc.log("ver = " + ver);
+            self._config = object.json;
+            cc.log("加载数据完成！");
         });      
     },
 
--- cocos_mojing/assets/script/menuUI.js
+++ cocos_mojing/assets/script/menuUI.js
@@ -14,13 +14,11 @@ cc.Class({
             type:cc.Button,
             tooltip: '主页'
         },
-
         Img_bag: {
             default:null, 
             type:cc.Button,
             tooltip: '锦囊'
         },
-
         Img_record: {
             default:null, 
             type:cc.Button,
@@ -36,31 +34,37 @@ cc.Class({
             type:cc.Node,
             tooltip: '选择'
         },
+        node_main: {
+            default:null, 
+            type:cc.Node,
+            tooltip: 'node_main'
+        },
+        node_bag: {
+            default:null, 
+            type:cc.Node,
+            tooltip: 'node_bag'
+        },
+        node_my: {
+            default:null, 
+            type:cc.Node,
+            tooltip: 'node_my'
+        },
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad () {
-        cc.log("onLoad");
+        cc.log("menuUI onLoad");
         this.addClickEvent(this.Img_main, this.node, "menuUI", "onImg_main");
         this.addClickEvent(this.Img_bag, this.node, "menuUI", "onImg_bag");
         this.addClickEvent(this.Img_record, this.node, "menuUI", "onImg_record");
         this.addClickEvent(this.Img_my, this.node, "menuUI", "onImg_my");
     },
-
-
     onImg_main(){        
         cc.log("onImg_main");
-        var pos = this.Img_main.node.getPosition();
-        this.Img_select.setPosition(pos);
+        this.setSelect(this.Img_main, this.node_main);
     },
-
     onImg_bag(){
         cc.log("onImg_bag");
-        var pos = this.Img_bag.node.getPosition();
-        this.Img_select.setPosition(pos);
+        this.setSelect(this.Img_bag, this.node_bag);
     },
-
     onImg_record(){
         cc.log("onImg_record");
         var pos = this.Img_record.node.getPosition();
@@ -70,6 +74,7 @@ cc.Class({
         cc.log("onImg_my");
         var pos = this.Img_my.node.getPosition();
         this.Img_select.setPosition(pos);
+        this.setSelect(this.Img_my, this.node_my);
     },
     addClickEvent: function(node,target,component,handler){
         var eventHandler = new cc.Component.EventHandler();
@@ -78,10 +83,19 @@ cc.Class({
         eventHandler.handler = handler;
         node.clickEvents.push(eventHandler);
     },
-
     start () {
-
+        cc.log("start");
+        this._select = this.node_main;
     },
-
-    // update (dt) {},
+    setSelect(pImg, pNode){
+        if (this._select == pNode) {
+            cc.log("same");
+            return;
+        }
+        var pos = pImg.node.getPosition();
+        this.Img_select.setPosition(pos);
+        this._select.active = false;
+        this._select = pNode;
+        this._select.active = true;
+    }
 });
